c887ca8888d8baaf095c9f77c67aa4098d0ee14c
yokekawa <yoekawa@gmail.com>
2026-04-20 13:26:26 +0900

    revised

--- src/App.tsx
+++ src/App.tsx
@@ -812,9 +812,9 @@ function AtBatForm({
         </optgroup>
         <optgroup label="進塁打・小技">
           <option value="犠牲フライ">犠牲フライ</option>
-          <option value="犠牲バント">犠牲バント</option>
-          <option value="セーフティバント">セーフティバント</option>
           <option value="送りバント">送りバント</option>
+          <option value="スクイズ">スクイズ</option>
+          <option value="セーフティスクイズ">セーフティスクイズ</option>
         </optgroup>
         <optgroup label="四死球・三振">
           <option value="空振り三振">空振り三振</option>
